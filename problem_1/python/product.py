9e64cfd1b7020964228ca8f063db2362b80173dc
Rowland Carlson <rowland@stillclever.com>
2020-07-07 09:15:39 -0500

    simple_product passes test

--- problem_1/python/product.py
+++ problem_1/python/product.py
@@ -1,2 +1,8 @@
-def simple_product():
-    pass
+from functools import reduce
+
+def simple_product(array):
+    product = reduce(lambda x, y: x * y, array)
+    result = []
+    for number in array:
+        result.append(product / number)
+    return result
